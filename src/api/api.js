2b1cf8b20cf034e70170667798993f948aae2ff7
TM <tm@macadmin.ru>
2017-03-29 11:17:39 +0300

    Store, add zones

--- src/api/api.js
+++ src/api/api.js
@@ -5,4 +5,8 @@ export default {
     return axios.post('http://dashboard-cars.dsk2.picompany.ru/api/v1/trackers')
     // return axios.post('http://localhost:3000/api/v1/trackers')
   }
+
+  getZones: function() {
+    return axios.get('http://dashboard-cars.dsk2.picompany.ru/api/v1/zones')
+  }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -14,16 +14,28 @@ const store = new Vuex.Store({
         status_label: '',
         zone_label: ''
       },
+    ],
+    zones: [
+      {
+        id: 1,
+        label: 'Нет связи с сервером API !'
+      }
     ]
   },
   mutations: {
     UPDATE_TRACKERS(state, trackers) {
       state.trackers = trackers
+    },
+    UPDATE_ZONES(state, zones) {
+      state.zones = zones
     }
   },
   actions: {
     refresh({commit}) {
       API.getTrackers().then(response => commit('UPDATE_TRACKERS', response.data))
+    },
+    refresh_zones({commit}) {
+      API.getZones().then(response => commit('UPDATE_ZONES', response.data))
     }
   }
 })

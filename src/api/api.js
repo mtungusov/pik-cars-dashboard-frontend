dff86adf84dbc1181dec350c0004872dcddbd134
TM <tm@macadmin.ru>
2017-04-27 17:07:44 +0300

    api url correct

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const URL = 'http://dashboard-cars.pik-industry.ru/'
+const URL = 'http://dashboard-cars.pik-industry.ru'
 // const URL = 'http://localhost:3000'
 
 export default {
